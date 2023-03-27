730b842ecdaf775ad7e9487e7f9865fffbd44017
조용진 <101401582+Brio-yj@users.noreply.github.com>
2023-03-27 17:57:32 +0900

    Create 2630.cpp
    .

---
+++ BackJjun/devide and conquer/2630/2630.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<string> intv;
+
+int main(){
+    int N,exponent,times;
+    cin>>N;
+    while(N--){
+        intv v;
+        cin>>exponent;
+        times=pow(3,exponent);
+        while(times--){
+            v.push_back("-");
+        }
+        if(v[0]==v[1]){
+            
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
