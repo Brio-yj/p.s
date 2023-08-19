5359e3fb03a5a752ffd8451505752d06b19c692d
조용진 <101401582+Brio-yj@users.noreply.github.com>
2023-08-19 09:23:32 +0900

    .

---
+++ MST/전력난.py
@@ -0,0 +1,43 @@
+def find(x):
+    if parent[x]!=x:
+        parent[x]=find(parent[x])
+    return parent[x]
+
+def union(a,b):
+    a=find(a)
+    b=find(b)
+
+    if a==b:
+        return 0
+    
+    if rank[a]<rank[b]:
+        parent[a]=b
+    else:
+        parent[b]=a
+        rank[a]+=1
+
+def kruskal(graph):
+    graph.sort(key=lambda x : x[2])
+    global parent
+    parent = list(range(len(graph)))
+    global rank
+    rank = [0] * len(graph)
+    sum = 0
+    for edge in graph:
+        s,e,w = edge
+        if union(s,e)!=0:
+            sum+=w
+    return sum
+
+n,m=map(int,input().split())
+
+graph=[]
+sum=0
+for i in range(m):
+    s,e,w=map(int,input().split())
+    graph.append((s,e,w))
+    sum+=w
+
+print(sum-kruskal(graph))
+
+
