fb200b0a9b15b74ab705fe8f1aa6d3d4cae1fee9
조용진 <101401582+Brio-yj@users.noreply.github.com>
2023-09-04 20:20:03 +0900

    .

--- Greedy/A와_B.py
+++ Greedy/A와_B.py
@@ -1,12 +1,34 @@
 import sys
-input = sys.stdin.readline
+A=sys.stdin.readline().rstrip()
+B=sys.stdin.readline().rstrip()
 
+alist=[]
+for i in A:
+    alist.append(i)
 
+blist=[]
+for i in B:
+    blist.append(i)
 
+cnt=len(blist)-len(alist)
+newcnt=2**cnt #8
+newlist=alist.copy()
+j=0
+for i in range(newcnt-1):
+    for j in range(cnt):
+        newlist.append("A")
+        if newlist==blist:
+            print(newlist)
+            print(1)
+            quit()
+        for k in range(cnt-j-1):
+            newlist.reverse()
+            newlist.append("B")
+            if newlist==blist:
+                print(newlist)
+                print(1)
+                quit()
+        newlist=alist.copy()
 
-
-
-
-
-
-
+print(newlist)
+print(0)
